13e7dd15a5274725911d566c7847a6ca60eb8116
40206446 <nmcmurray03@qub.ac.uk>
2025-04-25 13:27:08 +0100

    addition of Budget, expense and remaining placeholders for project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-    
+    <div className="container">
+      <h1 className="mt-3">Budgeting App</h1>
+      <div className= 'row mt-3'>
+        <div className="col-sm">
+          <Budget />
+        </div>
+        <div className="col-sm">
+          <Remaining />
+        </div>
+        <div className="col-sm">
+          <ExpenseTotal />
+        </div>
+      </div>
+    </div>
   );
-}
-
+};
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,16 @@ function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <MyButton />
     </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
   );
 }
 
